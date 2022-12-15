dcbce2fb8df7412979d61bc6b70ad3995db0821d
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2022-12-15 21:44:19 +0100

    Day 6, part 2

--- 6/solution.py
+++ 6/solution.py
@@ -6,10 +6,18 @@ def read_puzzle_input(puzzle_input: str) -> str:
 
 data = read_puzzle_input('puzzle_input.txt')
 
-window_len = 4
-for i in range(window_len - 1, len(data)):
-    window = data[i:i+window_len]
-    if len(set(window)) == window_len:
-        # solution 1
-        print("Solution 1: ", i + window_len)
-        break
+STARTOFPACKET = 4
+STARTOFMSG = 14
+
+MarkerEndPos = int
+
+def marker_lookup(marker_length: int) -> MarkerEndPos:
+    for i in range(marker_length - 1, len(data)):
+        window = data[i:i+marker_length]
+        if len(set(window)) == marker_length:
+            return  i + marker_length
+
+# solution 1
+print("Solution 1: ", marker_lookup(STARTOFPACKET))
+# solution 2
+print("Solution 2: ", marker_lookup(STARTOFMSG))
