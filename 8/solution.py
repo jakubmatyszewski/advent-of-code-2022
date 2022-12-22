214e8fbbc1375bba9e5bf6a54380f60a30243b65
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2022-12-22 15:52:45 +0100

    Day 8, part 2

--- 8/solution.py
+++ 8/solution.py
@@ -31,5 +31,26 @@ for i, line in enumerate(data[1:dim-1], 1):
 # solution 1
 print("Solution 1: ", visible)
 
-# # solution 2
-# print("Solution 2: ", score)
+def scenic_score(height, side):
+    side_score = [1 if height > x else 0 for x in side]
+    
+    try:
+        r = side_score.index(0) + 1
+    except ValueError:
+        r = len(side)
+
+    return r
+
+top_scenic_score = 0
+for i, line in enumerate(data[1:dim-1], 1):
+    for j, num in enumerate(line[1:dim-1], 1):
+        left = scenic_score(num, list(reversed(line[:j])))
+        right = scenic_score(num, line[j+1:])
+        up = scenic_score(num, list(reversed(data_t[j][:i])))
+        down = scenic_score(num, data_t[j][i+1:])
+        score = left * right * up * down
+
+        (top_scenic_score := max(score, top_scenic_score))
+
+# solution 2
+print("Solution 2: ", top_scenic_score)
