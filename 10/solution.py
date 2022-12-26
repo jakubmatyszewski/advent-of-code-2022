415f83581f358b580d89d4db44061b1518efd461
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2022-12-26 18:59:54 +0100

    Day 10, part 2

--- 10/solution.py
+++ 10/solution.py
@@ -9,15 +9,19 @@ data = read_puzzle_input('puzzle_input.txt')
 
 REGISTER = [20, 60, 100, 140, 180, 220]
 
-class CPU:
+
+class Computer:
     def __init__(self, data):
         self.cycle = 0
         self.x = 1
         self.output = 0
         self.data = data
+        self.crt = [['.'] * 40 for _ in range(6)]
+        
 
     def update_cycle(self):
         self.cycle += 1
+        self.gpu()
         if self.cycle in REGISTER:
             self.output += self.cycle * self.x
 
@@ -30,10 +34,24 @@ class CPU:
                 self.update_cycle()
                 self.update_cycle()
                 self.x += addx
-    
         return self.output
+    
+    def gpu(self):
+        column = (self.cycle - 1) % 40
+        row = int((self.cycle - 1) / 40)
+        sprite = [self.x -1, self.x, self.x + 1]
+        if column in sprite:
+            self.crt[row][column] = "#"
+        else:
+            self.crt[row][column] = "."
 
-computer = CPU(data)
+    def display(self):
+        print('\n'.join([''.join(line) for line in self.crt]))
+
+computer = Computer(data)
 
 # solution 1
 print("Solution 1: ", computer.run())
+
+# solution 2
+print("Solution 2: ", computer.display())
